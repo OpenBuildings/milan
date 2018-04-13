47a5225bf7a0d604565bf42c4a4a72a4ec317137
Haralan Dobrev <hkdobrev@gmail.com>
2018-04-13 21:00:50 +0300

    Consolidate deployment scripts with debug message about IP

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+openssl aes-256-cbc -K $encrypted_c5c534c0fd9c_key -iv $encrypted_c5c534c0fd9c_iv -in id_rsa.enc -out id_rsa -d
+
 for i in {1..2}; do
     SERVER_IP_VAR_NAME=SERVER${i}_IP
     IP=${!SERVER_VAR_NAME}
-    ssh -t clippings@$IP -i id_rsa "cd $DEPLOY_DIR && git fetch --depth=5 && git reset --hard $TRAVIS_TAG"
+    echo "Deploying to $IP..."
+    ssh -i id_rsa -t clippings@$IP "cd $DEPLOY_DIR && git fetch --depth=5 && git reset --hard $TRAVIS_TAG"
 done
