655bf762ab8baf66618ca92cfac7998d203451c6
Haralan Dobrev <hkdobrev@gmail.com>
2018-04-13 21:03:20 +0300

    Switch to local id_rsa file

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-openssl aes-256-cbc -K $encrypted_c5c534c0fd9c_key -iv $encrypted_c5c534c0fd9c_iv -in id_rsa.enc -out id_rsa -d
+openssl aes-256-cbc -K $encrypted_c5c534c0fd9c_key -iv $encrypted_c5c534c0fd9c_iv -in ./id_rsa.enc -out ./id_rsa -d
 
 for i in {1..2}; do
     SERVER_IP_VAR_NAME=SERVER${i}_IP
     IP=${!SERVER_VAR_NAME}
     echo "Deploying to $IP..."
-    ssh -i id_rsa -t clippings@$IP "cd $DEPLOY_DIR && git fetch --depth=5 && git reset --hard $TRAVIS_TAG"
+    ssh -i ./id_rsa -t clippings@$IP "cd $DEPLOY_DIR && git fetch --depth=5 && git reset --hard $TRAVIS_TAG"
 done
