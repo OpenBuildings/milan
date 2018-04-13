45f6695a81ed86100c09464cebf37c12e0d71ab0
Haralan Dobrev <hkdobrev@gmail.com>
2018-04-14 00:48:01 +0300

    Simplify deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,9 +4,5 @@ set -e
 
 openssl aes-256-cbc -K $encrypted_c5c534c0fd9c_key -iv $encrypted_c5c534c0fd9c_iv -in .travis/id_rsa.enc -out .travis/id_rsa -d
 
-for i in {1..2}; do
-    declare SERVER_IP_VAR_NAME="SERVER$(($i))_IP"
-    IP=${!SERVER_VAR_NAME}
-    echo "Deploying to $IP..."
-    ssh -i .travis/id_rsa -t clippings@$IP "cd $DEPLOY_DIR && git fetch --depth=5 && git reset --hard $TRAVIS_TAG"
-done
+ssh -i .travis/id_rsa -t clippings@$SERVER1_IP "cd $DEPLOY_DIR && git fetch --depth=5 && git reset --hard $TRAVIS_TAG"
+ssh -i .travis/id_rsa -t clippings@$SERVER2_IP "cd $DEPLOY_DIR && git fetch --depth=5 && git reset --hard $TRAVIS_TAG"
